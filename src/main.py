505c95b3600781d820bd073297bd2d03c9315343
ankardo <5149932+ankardo@users.noreply.github.com>
2024-04-29 10:19:52 -0300

    current state of the development

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+
+def main():
+    new_text_node = TextNode("This is a text node", "bold", "www.google.com")
+    print(new_text_node)
+
+
+if __name__ == "__main__":
+    main()
